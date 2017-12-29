495a6e0c8bf1c64add6e8c73bc8ceb610a61afc5
ChessMax <ChessMax@xakep.ru>
2017-12-29 14:54:07 +0300

    - Issue #127: Игнорировать html внутри ```

--- lpm-core/helper/HTMLHelper.php
+++ lpm-core/helper/HTMLHelper.php
@@ -21,19 +21,19 @@ class HTMLHelper
 	public static function codeIt($text)
 	{
     	return preg_replace_callback(
-    		"/(^|[^`])(?:`( [^\n]{1,} |[^`\n]{1,})`|```(\w{1,}\s*\n)?([^`].*?[^`])```)/ius", 
+    		"/(^|[^`])(?:`( [^\n]{1,} |[^`\n]{1,})`|```(\w{1,}\s*\n)?([^`].*?[^`])```)/ius",
     		function ($matches) {
     			if (empty($matches[2]))
     			{
     				$tag = 'pre';
-    				$text = trim($matches[4], "\n\r");
+    				$text = htmlentities(trim($matches[4], "\n\r"));
     				$classname = empty($matches[3]) ? 'nohighlight' : trim($matches[3]); 
     				$text = '<code class="' . $classname . '">' . $text . '</code>';
     			}
     			else 
     			{
     				$tag = 'code';
-    				$text = $matches[2];
+    				$text = htmlentities($matches[2]);
     			}
     			return $matches[1] . '<' . $tag . '>' . $text . '</' . $tag . '>';
     		}, $text);
