1fdfaeded18898ecf86fd39ac3c36904a7b428b4
greymag <greymag@gmail.com>
2018-02-02 16:34:42 +0300

    В экспорт добавлена колонка с датой завершения задачи.

--- lpm-core/project/export/IssuesExporterToExcel.php
+++ lpm-core/project/export/IssuesExporterToExcel.php
@@ -1,4 +1,6 @@
 <?php
+use \GMFramework\DateTimeUtils as DTU;
+
 /**
  * Класс, позволяющий экспортировать задачи в Excel файл.
  */
@@ -15,22 +17,28 @@ class IssuesExporterToExcel extends IssuesExporter {
 		$sheet = $doc->getActiveSheet();
 		$sheet->setTitle('Задачи');	
 
-		$sheet->setCellValue('A1', 'Задача');
-		$sheet->setCellValue('B1', 'SP'); // TODO: по идее надо проверять на scrum/не scrum
+		$sheet->setCellValue('A1', 'Дата завершения');
+		$sheet->setCellValue('B1', 'Задача');
+		$sheet->setCellValue('C1', 'SP'); // TODO: по идее надо проверять на scrum/не scrum
 
 		$row = 2;
 		foreach ($list as $issue) {
-			$sheet->setCellValue('A' . $row, $issue->name);
-			$sheet->setCellValue('B' . $row, $issue->hours);
+			$sheet->setCellValue('A' . $row, DTU::date('Y-m-d', $issue->completedDate));
+			$sheet->setCellValue('B' . $row, $issue->name);
+			$sheet->setCellValue('C' . $row, $issue->hours);
 			$row++;
 		}
 
 		// Устанавливаем формат, чтобы было красиво
 		$row--;
-		$sheet->getColumnDimension('A')->setWidth(100);
+		$sheet->getColumnDimension('A')->setAutoSize(true);
+		$sheet->getColumnDimension('B')->setAutoSize(true);
+		//$sheet->getColumnDimension('B')->setWidth(100);
 		$sheet->getStyle('A2:A' . $row)->getNumberFormat()->setFormatCode(
-			\PHPExcel_Style_NumberFormat::FORMAT_TEXT);
+			\PHPExcel_Style_NumberFormat::FORMAT_DATE_YYYYMMDD2);
 		$sheet->getStyle('B2:B' . $row)->getNumberFormat()->setFormatCode(
+			\PHPExcel_Style_NumberFormat::FORMAT_TEXT);
+		$sheet->getStyle('C2:C' . $row)->getNumberFormat()->setFormatCode(
 			\PHPExcel_Style_NumberFormat::FORMAT_NUMBER_00);
 
 		try {
