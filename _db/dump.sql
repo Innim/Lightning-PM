0d27a4a9091b5c4934a3b18c181a7bf943255b44
greymag <greymag@gmail.com>
2019-08-06 10:31:04 +0300

    Актуальный дамб БД.

--- _db/dump.sql
+++ _db/dump.sql
@@ -1,4 +1,4 @@
--- Adminer 4.1.0 MySQL dump
+-- Adminer 4.7.1 MySQL dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -30,6 +30,7 @@ CREATE TABLE `lpm_images` (
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'имя изображения',
   `desc` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'описание фото',
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'дата добавления',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`imgId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='загруженные пользователем изображения';
 
@@ -38,9 +39,10 @@ DROP TABLE IF EXISTS `lpm_issues`;
 CREATE TABLE `lpm_issues` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `projectId` int(11) NOT NULL,
+  `idInProject` int(11) NOT NULL,
   `parentId` int(11) NOT NULL DEFAULT '0' COMMENT 'идентификатор родительской задачи',
   `name` varchar(255) NOT NULL,
-  `hours` int(11) NOT NULL,
+  `hours` decimal(10,1) NOT NULL,
   `desc` text NOT NULL,
   `type` tinyint(1) NOT NULL DEFAULT '0',
   `authorId` bigint(19) NOT NULL,
@@ -59,9 +61,29 @@ DROP TABLE IF EXISTS `lpm_issue_counters`;
 CREATE TABLE `lpm_issue_counters` (
   `issueId` bigint(20) NOT NULL COMMENT 'идентификатор задачи',
   `commentsCount` int(8) NOT NULL DEFAULT '0' COMMENT 'количество комментариев',
-  `imgsCount` int(11) NOT NULL DEFAULT '0' COMMENT 'количество картинок, прикрепленных к задаче',
   PRIMARY KEY (`issueId`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='счетчики для задачи';
+
+
+DROP TABLE IF EXISTS `lpm_issue_labels`;
+CREATE TABLE `lpm_issue_labels` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор',
+  `projectId` int(11) NOT NULL DEFAULT '0' COMMENT 'Проект (0 если метка общая)',
+  `label` varchar(255) NOT NULL COMMENT 'Текст метки',
+  `countUses` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Количество использований',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `projectId` (`projectId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Метки для задач';
+
+
+DROP TABLE IF EXISTS `lpm_issue_member_info`;
+CREATE TABLE `lpm_issue_member_info` (
+  `instanceId` bigint(18) NOT NULL COMMENT 'идентификатор задачи',
+  `userId` bigint(18) NOT NULL COMMENT 'идентификатор пользователя',
+  `sp` decimal(10,1) NOT NULL COMMENT 'Значения SP за задачу',
+  PRIMARY KEY (`instanceId`,`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='информация об участнике задачи';
 
 
 DROP TABLE IF EXISTS `lpm_members`;
@@ -81,14 +103,14 @@ CREATE TABLE `lpm_options` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='таблица настроек';
 
 INSERT INTO `lpm_options` (`option`, `value`) VALUES
-('cookieExpire',	'30'),
-('currentTheme',	'default'),
-('title',	'FlaPS Group'),
-('subtitle',	'Менеджер проектов'),
-('logo',	'imgs/lightning-logo.png'),
-('fromEmail',	'donotreply@flaps.ru'),
-('fromName',	'FlaPS LLC PM'),
-('emailSubscript',	'Это письмо отправлено автоматически, не отвечайте на него. \r\nВы можете отключить отправку уведомлений в настройках профиля');
+('cookieExpire',  '30'),
+('currentTheme',  'default'),
+('title', 'Innim LLC'),
+('subtitle',  'Менеджер проектов'),
+('logo',  'imgs/lightning-logo.png'),
+('fromEmail', 'donotreply@innim.ru'),
+('fromName',  'Innim LLC PM'),
+('emailSubscript',  'Это письмо отправлено автоматически, не отвечайте на него. \r\nВы можете отключить отправку уведомлений в настройках профиля');
 
 DROP TABLE IF EXISTS `lpm_projects`;
 CREATE TABLE `lpm_projects` (
@@ -99,27 +121,58 @@ CREATE TABLE `lpm_projects` (
   `date` datetime NOT NULL,
   `lastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'время последнего обновления проекта',
   `issuesCount` int(9) NOT NULL DEFAULT '0' COMMENT 'количество созданных задач',
+  `scrum` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'Проект использует Scrum',
+  `isArchive` tinyint(1) NOT NULL DEFAULT '0',
+  `slackNotifyChannel` varchar(255) NOT NULL COMMENT 'имя канала для оповещений в Slack',
+  `masterId` bigint(19) NOT NULL COMMENT 'идентификатор пользователя, являющегося мастером в проекте',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uid` (`uid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
-DROP TABLE IF EXISTS `lpm_tags`;
-CREATE TABLE `lpm_tags` (
-  `tagId` bigint(18) NOT NULL COMMENT 'идентификатор тега',
-  `itemId` bigint(18) NOT NULL COMMENT 'идентификатор инстанции',
-  PRIMARY KEY (`tagId`,`itemId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='теги, привязанные к инстанциям';
+DROP TABLE IF EXISTS `lpm_recovery_emails`;
+CREATE TABLE `lpm_recovery_emails` (
+  `id` bigint(19) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `userId` bigint(19) NOT NULL,
+  `recoveryKey` varchar(255) NOT NULL,
+  `expDate` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userId` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-DROP TABLE IF EXISTS `lpm_tags_list`;
-CREATE TABLE `lpm_tags_list` (
-  `tagId` bigint(18) NOT NULL AUTO_INCREMENT COMMENT 'идентификатор тега',
-  `itemType` tinyint(2) NOT NULL COMMENT 'тип элементов, к которым применим тег',
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'название тега',
-  PRIMARY KEY (`tagId`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='теги';
+DROP TABLE IF EXISTS `lpm_scrum_snapshot`;
+CREATE TABLE `lpm_scrum_snapshot` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор записи',
+  `sid` int(11) NOT NULL COMMENT 'Идентификатор snapshot-а',
+  `issue_uid` int(11) NOT NULL COMMENT 'Глобавльный идентификатор задачи',
+  `issue_pid` int(11) NOT NULL COMMENT 'Идентификатор задачи в проекте',
+  `issue_name` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT 'Название задачи',
+  `issue_state` tinyint(2) NOT NULL COMMENT 'Состояние задачи',
+  `issue_sp` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT 'Количество SP',
+  `issue_members_sp` text CHARACTER SET utf8 NOT NULL COMMENT 'Количество SP по участникам',
+  `issue_priority` tinyint(2) NOT NULL COMMENT 'Приоритет задачи',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `lpm_scrum_snapshot_list`;
+CREATE TABLE `lpm_scrum_snapshot_list` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор snapshot-а',
+  `idInProject` int(11) NOT NULL COMMENT 'Порядковый номер снепшота по проекту',
+  `pid` int(11) NOT NULL COMMENT 'Идентификатор проекта',
+  `creatorId` bigint(19) NOT NULL COMMENT 'Идентификатор создателя snapshot-а',
+  `created` datetime NOT NULL COMMENT 'Время создания snapshot-а',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `lpm_scrum_sticker`;
+CREATE TABLE `lpm_scrum_sticker` (
+  `issueId` bigint(18) NOT NULL COMMENT 'идентификатор задачи',
+  `state` tinyint(2) NOT NULL COMMENT 'состояние стикера',
+  PRIMARY KEY (`issueId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='стикер на scrum доске';
 
 
 DROP TABLE IF EXISTS `lpm_users`;
@@ -127,20 +180,19 @@ CREATE TABLE `lpm_users` (
   `userId` bigint(19) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `pass` varchar(255) NOT NULL,
-  `cookieHash` varchar(32) NOT NULL DEFAULT '' COMMENT 'хеш для переавторизации по кукам',
   `firstName` varchar(128) NOT NULL,
   `lastName` varchar(128) NOT NULL,
   `nick` varchar(255) NOT NULL,
   `lastVisit` datetime NOT NULL,
   `regDate` datetime NOT NULL,
   `role` tinyint(1) NOT NULL DEFAULT '0',
+  `locked` tinyint(1) NOT NULL DEFAULT '0',
   `secret` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'скрытый пользователь',
+  `slackName` varchar(255) NOT NULL COMMENT 'имя в Slack',
   PRIMARY KEY (`userId`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO `lpm_users` (`userId`, `email`, `pass`, `cookieHash`, `firstName`, `lastName`, `nick`, `lastVisit`, `regDate`, `role`, `secret`) VALUES
-(14,	'test@test.com',	'$1$V7A9iC1B$3JkJzDQLyGIAdSWCjGwm8/',	'937f8fc04ad180df25d9feabbc741b05',	'Test',	'Test',	'test',	'2014-10-10 11:35:10',	'2014-10-10 11:35:00',	0,	0);
 
 DROP TABLE IF EXISTS `lpm_users_pref`;
 CREATE TABLE `lpm_users_pref` (
@@ -152,8 +204,18 @@ CREATE TABLE `lpm_users_pref` (
   PRIMARY KEY (`userId`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='настройки пользователя';
 
-INSERT INTO `lpm_users_pref` (`userId`, `seAddIssue`, `seEditIssue`, `seIssueState`, `seIssueComment`) VALUES
-(14,	1,	0,	1,	1);
+
+DROP TABLE IF EXISTS `lpm_user_auth`;
+CREATE TABLE `lpm_user_auth` (
+  `id` bigint(18) NOT NULL AUTO_INCREMENT,
+  `cookieHash` varchar(32) NOT NULL,
+  `userAgent` varchar(255) NOT NULL COMMENT 'информация о браузере юзера',
+  `userId` bigint(18) NOT NULL COMMENT 'индентификатор пользователя',
+  `hasCreated` datetime NOT NULL COMMENT 'дата создания записи',
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Данные авторизации по куки';
+
 
 DROP TABLE IF EXISTS `lpm_workers`;
 CREATE TABLE `lpm_workers` (
@@ -183,3 +245,6 @@ CREATE TABLE `lpm_work_study` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `workerId` (`workerId`,`date`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- 2019-08-06 07:24:23
