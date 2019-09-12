d64011c7c150a87eb8d3a2f61a6dd11b8e3c206b
David <David.perov60@gmail.com>
2019-09-12 09:32:30 +0300

    Правка - Поправлен баг с восстановлением пароля.

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 // инициализация
 require_once( dirname( __FILE__ ) . '/lpm-core/init.inc.php' );
 
-//echo "<pre/>";
-//var_dump($_GET);
-//var_dump($this->getParam(0));
-//exit;
-
 $lightning = new LightningEngine();
 $lightning->createPage();
 ?>
