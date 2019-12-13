45544cf4c4dbe20fde6ccda1e30c190c9699453e
uzul <mhvdm03@gmail.com>
2019-12-13 15:05:55 +0300

    [CI] test git pull

--- ci/src/pm_deploy.py
+++ ci/src/pm_deploy.py
@@ -7,19 +7,16 @@ from pathlib import Path
 from shutil import copy2, copytree
 
 
-class Deployer(SshWorker):
-    def __init__(self, ssh_info: SshInfo, remote_app_dir):
+class PMDeployer(SshWorker):
+    def __init__(self, ssh_info: SshInfo, upload_path, remote_app_path):
         super().__init__(ssh_info)
-        self.remote_app_dir = remote_app_dir
+        self.upload_dir = upload_path
+        self.remote_app_dir = remote_app_path
         # self.src_dir = Path(src_dir).resolve()
         # self.deploy_dir = Path(self.src_dir).resolve()
 
     def deploy(self):
-        cmd = f'cd {self.remote_app_dir} && pwd && git checkout master && git pull'
+        self.connect()
+        cmd = f'cd {self.upload_dir} && pwd && git checkout master && git pull'
+        self.ssh_cmd(cmd)
         
-
-
-
-
-
-
--- ci/src/pm_worker.py
+++ ci/src/pm_worker.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from ciutil.core.worker import CIWorker
 from ciutil.deploy.ssh_worker import SshInfo
-from src.deploy import Deployer
+from src.pm_deploy import PMDeployer
 from src.pm_info import PMInfo
 
 
@@ -12,5 +12,12 @@ class PMWorker(CIWorker):
 
     def deploy(self):
         print('deploying...')
-        ssh_info = SshInfo(host=self.info.deploy_host)
-        deployer = Deployer()
+        ssh_info = SshInfo(host=self.info.deploy_host,
+                           port=self.info.deploy_port,
+                           user=self.info.deploy_user,
+                           password=self.info.deploy_password)
+        deployer = PMDeployer(ssh_info=ssh_info,
+                              upload_path=self.info.deploy_upload_path,
+                              remote_app_path=self.info.deploy_app_path)
+
+        deployer.deploy()
