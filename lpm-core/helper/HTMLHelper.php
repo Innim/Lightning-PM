086cba3fa3a05689dd315d74388c6b4293f54806
greymag <greymag@gmail.com>
2020-01-17 16:36:52 +0300

    Рефакторинг.

--- lpm-core/helper/HTMLHelper.php
+++ lpm-core/helper/HTMLHelper.php
@@ -1,13 +1,11 @@
 <?php
-class HTMLHelper 
-{
+class HTMLHelper {
 	/**
 	 * Автоматически заменяет url'ы в тексте на HTML ссылки
 	 * @param  string $text 
 	 * @return string
 	 */
-	public static function linkIt($text)
-	{
+	public static function linkIt($text) {
     	return preg_replace(
     		"/(https?:\/\/[^<\s]+[[:alnum:]])([^[:alnum:]]*(?:<br ?\/?>)*[^a-zа-я0-9]|\s|$)/iu", 
     		'<a href="$1" target="_blank">$1</a>$2', $text);
@@ -23,22 +21,20 @@ class HTMLHelper
 	public static function codeIt($text, $htmlEncode = true) {
         return self::processCode($text,
     		function ($matches) use ($htmlEncode) {
-    			if (empty($matches[2]))
-    			{
+    			if (empty($matches[2])) {
     				$tag = 'pre';
                     $text = trim($matches[4], "\n\r");
                     if ($htmlEncode)
     				    $text = htmlentities($text);
     				$classname = empty($matches[3]) ? 'nohighlight' : trim($matches[3]); 
     				$text = '<code class="' . $classname . '">' . $text . '</code>';
-    			}
-    			else 
-    			{
+    			} else {
     				$tag = 'code';
                     $text = $matches[2];
                     if ($htmlEncode)
                         $text = htmlentities($text);
     			}
+
     			return $matches[1] . '<' . $tag . '>' . $text . '</' . $tag . '>';
     		});
 	}
@@ -52,20 +48,16 @@ class HTMLHelper
     public static function stripCode($text) {
         return self::processCode($text,
             function ($matches) {
-                if (empty($matches[2]))
-                {
+                if (empty($matches[2])) {
                     $text = '';
-                }
-                else 
-                {
+                } else {
                     $text = htmlentities($matches[2]);
                 }
                 return $matches[1] . $text;
             });
     }
 
-    private static function processCode($text, $func)
-    {
+    private static function processCode($text, $func) {
         return preg_replace_callback(
             "/(^|[^`])(?:`( [^\n]{1,} |[^`\n]{1,})`|```(\w{1,}\s*\n)?([^`].*?[^`])```)/ius",
             $func, $text);
