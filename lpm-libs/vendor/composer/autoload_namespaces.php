6b18aba031c4c740068807035bc68d787a16db19
greymag <greymag@gmail.com>
2020-01-17 16:30:32 +0300

    Подключил библиотеку Parsedown.

--- lpm-libs/vendor/composer/autoload_namespaces.php
+++ lpm-libs/vendor/composer/autoload_namespaces.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Parsedown' => array($vendorDir . '/erusev/parsedown'),
     'Evenement' => array($vendorDir . '/evenement/evenement/src'),
     'Devristo\\Phpws\\' => array($vendorDir . '/devristo/phpws/src'),
 );
