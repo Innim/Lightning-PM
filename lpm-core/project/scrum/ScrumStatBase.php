f1f829e36921a8653d2ed78b13219886ce3a6ca1
greymag <greymag@gmail.com>
2020-03-13 20:16:31 +0300

    В статистике выводится временной промежуток учтенных спринтов и количество недель.

--- lpm-core/project/scrum/ScrumStatBase.php
+++ lpm-core/project/scrum/ScrumStatBase.php
@@ -5,10 +5,12 @@
  * Эта статистика по спринтам может быть привязана к любой сущности: 
  * пользователь, проект и тп.
  */
-abstract class ScrumStatBase {
+abstract class ScrumStatBase extends LPMBaseObject {
 	protected $_snapshots = [];
 
 	private $_cachedTotalSp;
+	private $_periodStart;
+	private $_periodEnd;
 
 	/**
 	 * Количество SP, которое должно отображаться в статистике.
@@ -30,9 +32,43 @@ abstract class ScrumStatBase {
 	 * @param ScrumStickerSnapshot $snapshot [description]
 	 */
 	public function addSnapshot(ScrumStickerSnapshot $snapshot) {
+		if (empty($this->_periodEnd) || $snapshot->created > $this->_periodEnd)
+			$this->_periodEnd = $snapshot->created;
+
+		if (!empty($snapshot->started) &&
+				(empty($this->_periodStart) || $snapshot->started < $this->_periodStart))
+			$this->_periodStart = $snapshot->started;
+
 		$this->_snapshots[] = $snapshot;
 
 		$this->_cachedTotalSp = null;
+	}
+
+	/**
+	 * Возвращает форматированную строку для вывода периода, за который записана статистика.
+	 * @return string
+	 */
+	public function getPeriod() {
+		return empty($this->_periodStart) || empty($this->_periodEnd)
+			? '-'
+			: self::getDateStr($this->_periodStart) . ' - ' . self::getDateStr($this->_periodEnd);
+	}
+
+	/**
+	 * Возвращает количество недель, укладывающихся в период.
+	 * @return int
+	 */
+	public function getWeeksCount() {
+		if (empty($this->_periodStart) || empty($this->_periodEnd))
+			return 0;
+
+		$start = new DateTime();
+		$start->setTimestamp($this->_periodStart);
+
+		$end = new DateTime();
+		$end->setTimestamp($this->_periodEnd);
+
+		return round($start->diff($end)->days / 7);
 	}
 
 	/**
