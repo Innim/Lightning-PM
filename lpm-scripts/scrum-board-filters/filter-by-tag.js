f88648e5e3cff5f8d9a117129b2555d0bf7f31c8
GusNitrous <gusnitrous@gmail.com>
2021-11-22 15:10:44 +0300

    Refactoring: стиль отображения компонента фильтров; функция получения списка тегов

--- lpm-scripts/scrum-board-filters/filter-by-tag.js
+++ lpm-scripts/scrum-board-filters/filter-by-tag.js
@@ -1,12 +1,11 @@
 export function initFilters() {
-    Vue.component('vue-multiselect', window.VueMultiselect.default)
-
-    const filtersComponent = new Vue({
+    Vue.component('vue-multiselect', window.VueMultiselect.default);
+    const filters = new Vue({
         el: '#scrumBoardFilters',
         data: {
             message: 'Фильтры компонента',
             value: null,
-            options: ['list', 'of', 'options']
-        }
-    })
+            options: []
+        },
+    });
 }
