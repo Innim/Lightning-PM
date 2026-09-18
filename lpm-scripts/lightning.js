2b533c6f046bb281d3ee7a5d35ed3a3cba58c559
greymag <greymag@gmail.com>
2026-09-18 15:21:12 +0200

    feat(issues): copy an issue link from the lists and the board

--- lpm-scripts/lightning.js
+++ lpm-scripts/lightning.js
@@ -1275,8 +1275,6 @@ $(document).ready(
         });
 
         window.lpInfo.userId = $('#curUserId').val();
-        // Инициализация копирования в буфер
-        (new ClipboardJS('.copy-commit-message'));
     }
 );
 
